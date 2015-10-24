9c34065e6ddb57d758da286e44681f3c0d7a7876
jessheisme <jessheisme@berkeley.edu>
2015-10-24 13:39:17 -0700

    simplifed

--- drizzy/assets/script/script.js
+++ drizzy/assets/script/script.js
@@ -1,62 +1,35 @@
 $(document).ready(function() {
 
-	var naked = true;
 
 	$(".outfit-selection").click(function() {
+		// Hide the body and any activated outfits
 		$("#body").hide();
 		$(".outfit").hide();
-		naked = false;
+		// Get the last character of outfit-selection id
+		// which is the number corresponding to the outfit
+		var number = $(this).attr('id').slice(-1);
+		// Show the corresponding outfit
+		$("#o" + number).show();
 	})
 
-	$(".outfit").hover(function() {
-		$("#body").show();
-		$(active_outfit).hide();
-	})
-	$("#drake-container").hover(function() {
-		if (naked == false) {
-			$("#body").hide();
-			$(active_outfit).show();
-		}
-	})
-	
-
-	var active_outfit;
+/* 	
+	This is an alternative way of doing the same thing as above.
 	$("#s1").click(function() {
-		active_outfit = $("#o1");
 		$("#o1").show();
 	})
 	$("#s2").click(function() {
-		active_outfit = $("#o2");
 		$("#o2").show();
 	})
 	$("#s3").click(function() {
-		active_outfit = $("#o3");
 		$("#o3").show();
 	})
 	$("#s4").click(function() {
-		active_outfit = $("#o4");
 		$("#o4").show();
 	})
 	$("#s5").click(function() {
-		active_outfit = $("#o5");
 		$("#o5").show();
 	})
+*/
 
-	/*
-	$("#s1").hover(function() {
-		$("#o1").show();
-	})
-	$("#s2").hover(function() {
-		$("#o2").show();
-	})
-	$("#s3").hover(function() {
-		$("#o3").show();
-	})
-	$("#s4").hover(function() {
-		$("#o4").show();
-	})
-	$("#s5").hover(function() {
-		$("#o5").show();
-	})
-	*/
+
 })
