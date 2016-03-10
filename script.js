1b4d55862616e21f45b0b35d24e41a578dac0639
Jessica He <jessheisme@gmail.com>
2016-03-10 04:59:53 -0800

    issues

--- script.js
+++ script.js
@@ -51,7 +51,7 @@ $(document).ready(function() {
 
 	$(".project-preview h1 a").mouseenter(function() {
 		var number = $(this).attr('id').slice(-1);
-		$(".project-thumbnail video").fadeOut();
+		$(".project-thumbnail video, img").fadeOut();
 		$("#v" + number).css("display","block");
 	});
 
@@ -78,55 +78,4 @@ $(document).ready(function() {
 	});
 */
 
-	/***** STUFF FOR QUAK *****/
-
-	setTimeout(function() {
-		$("#button-container").show();
-		$("#arrow-right-container").fadeIn();
-	}, 5000)
-
-	$("#button-a").click(function() {
-		$("#answers").show();
-		$("#button-container").hide();
-		$("#back1").show();
-		$("#answer-em-button").show();
-
-		// if button-a clicked after clicking back1
-		$("#home-still").hide();
-	})
-
-	$("#button-q").mouseenter(function() {
-		$("#temp").fadeIn();
-	})
-	$("#button-q").mouseleave(function() {
-		$("#temp").fadeOut();
-	})
-
-	$("#back1").click(function() {
-		$("#home-still").show();
-		$("#answer-em-button").hide();
-		$("#answers").show();
-		$("#button-container").show();
-
-		// if back1 clicked after back2
-		$("#answers-still").hide();
-	})
-{}
-	$("#answer-em-button").click(function() {
-		$("#answer-em").show();
-		$("#back1").hide();
-		$("#back2").show();
-		$("#answer-em-button").hide();
-
-		// if answer-em-button clicked after clicking back2
-		$("#answers-still").hide();
-	})
-
-	$("#back2").click(function() {
-		$("#answers-still").show();
-		$("#answer-em-button").show();
-		$("#back2").hide();
-		$("#back1").show();
-		$("#answer-em").hide();
-	})
 })
