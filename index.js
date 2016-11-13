dd091d4b0f90d186769f70e29376b7cd6e5c7cf6
jessica He <jessheisme@berkeley.edu>
2016-11-13 15:31:37 -0800

    bux fixes that i think i fixed but how can you be so sure???????
    TDD TDD

--- index.js
+++ index.js
@@ -17,9 +17,14 @@ $(document).ready(function() {
 	
 	// STUFF HAPPENING ON SCROLL
 	$(window).scroll(function() {
-
+		// distance from top of window
 		var scrollTop = $(window).scrollTop();
-		var imageTop = $(".first-image").offset().top;
+		try {
+			var imageTop = $(".first-image").offset().top;
+		}
+		catch(e) {
+			$("#i1").addClass("first-image");
+		}
 		var distance = imageTop - scrollTop;
 		// console.log(distance);
 		// console.log("marker" + marker);
@@ -28,37 +33,39 @@ $(document).ready(function() {
 		var prevImage = $(".first-image").prev();
 
 
-		// sticky project title
+		
 		if (scrollTop >= 400) {
-			/***** BAD CODE CAN WRITE BETTER BUT DONT KNOW HOW RIGHT NOW *****/
+			// sticky project title
 			$("body #project-title-container").css("position","fixed");
 			$("body #project-title-container").css("top","100px");
 			$("#left-sidebar-container").fadeIn();
-		}
-		// unstick project title
-		if (scrollTop < 400) {
-			/***** BAD CODE CAN WRITE BETTER BUT DONT KNOW HOW RIGHT NOW *****/
-			$("body #project-title-container").css("position","absolute");
-			$("body #project-title-container").css("top","500px");
-			$("#left-sidebar-container").fadeOut();
+
+			// get and change project name
+			var projectNumber = $(".first-image").attr('id').slice(-1);
+			var projectID = $("#p" + projectNumber);
+			var projectName = projectID.html();
+			$("#p0").text(projectName);
 		}
 
+		// unstick project title
+		if (scrollTop < 400) {
+			$("body #project-title-container").css("position","absolute");
+			$("body #project-title-container").css("top","400px");
+			$("#left-sidebar-container").fadeOut();
+		}
+		console.log("distance: " + distance + "\nscrollTop: " + scrollTop + "\nmarker: " + marker);
 		// change project name scrolling down
 		if (distance < -300 && scrollTop > 0) {
+			console.log("change1");
 			$(".first-image").removeClass("first-image");
 			nextImage.addClass("first-image");
 		}
 		// change project name scrolling up
 		if (distance >= marker + 20 && scrollTop > 0) {
+			console.log("change2");
 			$(".first-image").removeClass("first-image");
 			prevImage.addClass("first-image");
-			$("#intro").fadeOut();
 		}
-		// get and change project name
-		var projectNumber = $(".first-image").attr('id').slice(-1);
-		var projectID = $("#p" + projectNumber);
-		var projectName = projectID.html();
-		$("#p0").text(projectName);
 
 		$(".project-title").hover(function() {
 			$("#i" + $(".first-image").attr('id').slice(-1)).css("opacity","1");
